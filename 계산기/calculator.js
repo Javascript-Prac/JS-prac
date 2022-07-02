c6d12f7f4e623e10b28f7504a774df3c91cfa86c
Jihoon Chae <93469760+jihoon-chae@users.noreply.github.com>
2022-07-02 22:26:26 +0900

    함수의 중괄호와 return 생략

--- 계산기/calculator.js
+++ 계산기/calculator.js
@@ -3,16 +3,14 @@ let operator = " ";
 let numTwo = " ";
 const $operator = document.querySelector("#operator");
 const $result = document.querySelector("#result");
-const onClickNumber = (number) => {
-  return () => {
-    if (operator) {
-      // 비어있다.
-      numTwo += number;
-    } else {
-      // 비어있지 않다.
-      numOne += number;
-    }
-  };
+const onClickNumber = (number) => () => {
+  if (operator) {
+    // 비어있다.
+    numTwo += number;
+  } else {
+    // 비어있지 않다.
+    numOne += number;
+  }
 };
 
 // 번호를 누르면 해당 번호를 문자열 상태로 더해주기
