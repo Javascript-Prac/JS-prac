e1cb6bcc178b088770e7450bb53e9e38f6df9bb3
Jihoon Chae <93469760+jihoon-chae@users.noreply.github.com>
2022-07-02 22:21:49 +0900

    함수 중복시 매개변수로 빼기

--- 계산기/calculator.js
+++ 계산기/calculator.js
@@ -1,84 +1,29 @@
-let numOne = " "
+let numOne = " ";
 let operator = " ";
-let numTwo = " "
+let numTwo = " ";
 const $operator = document.querySelector("#operator");
 const $result = document.querySelector("#result");
-// const onClickNumber = 
+const onClickNumber = (number) => {
+  if (operator) {
+    numTwo += number;
+  } else {
+    numOne += number;
+  }
+};
 
 // 번호를 누르면 해당 번호를 문자열 상태로 더해주기
-document.querySelector("#num-0").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-
-document.querySelector("#num-1").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-document.querySelector("#num-2").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-document.querySelector("#num-3").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-document.querySelector("#num-4").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-document.querySelector("#num-5").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-document.querySelector("#num-6").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-document.querySelector("#num-7").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-document.querySelector("#num-8").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-document.querySelector("#num-9").addEventListener("click", (number) => {
-  if (operator) {
-    numTwo += number;
-  } else {
-    numOne += number;
-  }
-});
-document.querySelector("#plus").addEventListener("click", (number) => {});
-document.querySelector("#minus").addEventListener("click", (number) => {});
-document.querySelector("#multiply").addEventListener("click", (number) => {});
-document.querySelector("#calculate").addEventListener("click", (number) => {});
-document.querySelector("#clear").addEventListener("click", (number) => {});
+document.querySelector("#num-0").addEventListener("click", "0");
+document.querySelector("#num-1").addEventListener("click", "1");
+document.querySelector("#num-2").addEventListener("click", "2");
+document.querySelector("#num-3").addEventListener("click", "3");
+document.querySelector("#num-4").addEventListener("click", "4");
+document.querySelector("#num-5").addEventListener("click", "5");
+document.querySelector("#num-6").addEventListener("click", "6");
+document.querySelector("#num-7").addEventListener("click", "7");
+document.querySelector("#num-8").addEventListener("click", "8");
+document.querySelector("#num-9").addEventListener("click", "9");
+document.querySelector("#plus").addEventListener("click", () => {});
+document.querySelector("#minus").addEventListener("click", () => {});
+document.querySelector("#multiply").addEventListener("click", () => {});
+document.querySelector("#calculate").addEventListener("click", () => {});
+document.querySelector("#clear").addEventListener("click", () => {});
